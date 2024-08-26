81edd7d6737e0625a1d374ee8ec0dd6105cdeb1b
Basala Valentyn <valikbasala@gmail.com>
2024-08-26 17:57:20 +0300

    edit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("!!!");
         System.out.println("323323");
+        System.out.println("kjfksnfek");
     }
 }
