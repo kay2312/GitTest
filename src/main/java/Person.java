ae2aac42fbd1efd0825b9a31aa077314bf40b805
Basala Valentyn <valikbasala@gmail.com>
2024-08-26 16:01:59 +0300

    Delete class Person

--- src/main/java/Person.java
+++
@@ -1,9 +0,0 @@
-public class Person {
-<<<<<<< HEAD
-=======
-    public static void main(String[] args) {
-        System.out.println("dkfidjfk");
-        System.out.println("Good");
-    }
->>>>>>> feature
-}
